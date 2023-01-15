9ec649ddb9b3d7f2bf1ee1c0091d11e2b4169250
Aleksander Górecki <goreckialeksander9@gmail.com>
2023-01-15 23:48:29 +0100

    beginning work on house details

--- src/components/houseDetails.js
+++ src/components/houseDetails.js
@@ -1,23 +1,63 @@
-import {Paper} from '@mui/material';
+import {Paper, Typography} from '@mui/material';
 import { Container } from '@mui/system';
 import { userType, getCurrentUser } from '../userType';
 import { useParams } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export const HouseDetails = () => {
 
-    const houseNotFoundMessage = "Dom o podanym Id nie istnieje. Wróć na poprzednią stronę i spróbuj ponownie."
+    const config = {
+        headers: {
+            'content-type': 'application/json',
+        }
+    };
+
     const params = useParams();
     const houseId = params.id;
+    const [houseName, setHouseName] = useState(null);
+    const [waterMeter, setWaterMeter] = useState(null);
+    const [electricityMeter, setElectricityMeter] = useState(null);
+    const [notFound, setNotFound] = useState(false);
     
     useEffect( () => {
-
+        axios
+        .get(`/api/House/getHouse?id=${houseId}`, config)
+        .then( (res) => {
+            if (res.status === 200) {
+                setHouseName(res.data.name);
+                axios
+                .get('/api/House/getAllHouses', config)
+                .then( (res1) => {
+                    setWaterMeter(res1.data.waterMeter)
+                    setElectricityMeter(res1.data.electricityMeter)
+                })
+                .catch((error) => {
+                    console.log(error)
+            });
+            }
+            else {
+                setNotFound(true);
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+    });
     }, [])
 
     return(
         <Container maxWidth='md'>
             <Paper sx={{p: 2}}>
-                asdasd
+                {!notFound ? (
+                    <Typography>
+                        Odczyty dla domu {houseName} (Id {houseId})
+                    </Typography>
+                ) : (
+                    <Typography>
+                        Dom o podanym Id nie istnieje. Wróć na poprzednią stronę i spróbuj ponownie.
+                    </Typography>
+                )}
+
             </Paper>
         </Container>
     )
